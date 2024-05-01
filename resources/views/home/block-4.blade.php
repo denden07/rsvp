82c0c5580517838e85b126cd93baadaff8a7dc01
denden07 <ilovenjr21@gmail.com>
2024-05-01 23:09:32 +0800

    general update

--- resources/views/home/block-4.blade.php
+++ resources/views/home/block-4.blade.php
@@ -1,199 +1,175 @@
 <div class="block-4-container">
     <div class="spacer-40"></div>
     <div class="gtco-testimonials">
-  <div class="owl-carousel owl-carousel1 owl-theme">
-    <div>
-      <div class="card text-center card-maroon">
-      <img  class="card-img-top" src="{{ asset('images/roses.jpg') }}" alt="" loading="lazy">
-
-        <div class="card-body">
-          <h5 class="text-gold">18 Roses </h5>
-          <div class="spacer-32"></div>
-          <div class="participants">
-            <ul>
-                <li class="card-text">Mr. Peregrino Tomas</li>
-                <li class="card-text">Mr. Julio Tomas</li>
-                <li class="card-text">Mr. Santos Pascual</li>
-                <li class="card-text">Mr. Alexander Tomas</li>
-                <li class="card-text">Mr. Ernesto Tomas</li>
-                <li class="card-text">Mr. Rogelio Tomas</li>
-                <li class="card-text">Mr. Melanio Tomas</li>
-                <li class="card-text">Mr. Generoso Tomas</li>
-                <li class="card-text">Gerard Ashlee Jose</li>
-            </ul>
-
-            <ul>
-                <li class="card-text">Sebastian Gabriel Co</li>
-                <li class="card-text">Kean Henderson Tan</li>
-                <li class="card-text">Waylan Kantrel Guiang</li>
-                <li class="card-text">Rham Axcel Acoba</li>
-                <li class="card-text">Alexander James Acebo</li>
-                <li class="card-text">Charl Rotsen Lucero</li>
-                <li class="card-text">Jathniel Manangbao</li>
-                <li class="card-text">Joshua Nashley Pascua</li>
-                <li class="card-text">Mr. Uldarico Rosario Jr.</li>
-            </ul>
-          </div>
+        <div class="owl-carousel owl-carousel1 owl-theme">
+            <div>
+                <div class="card text-center card-maroon">
+                    <img class="card-img-top" src="{{ asset('images/roses.jpg') }}" alt="" loading="lazy">
+                    <div class="card-body">
+                        <h5 class="text-gold">18 Roses </h5>
+                        <div class="spacer-32"></div>
+                        <div class="participants">
+                            <ul>
+                                <li class="card-text">• Mr. Peregrino Tomas</li>
+                                <li class="card-text">• Mr. Julio Tomas</li>
+                                <li class="card-text">• Mr. Santos Pascual</li>
+                                <li class="card-text">• Mr. Alexander Tomas</li>
+                                <li class="card-text">• Mr. Ernesto Tomas</li>
+                                <li class="card-text">• Mr. Rogelio Tomas</li>
+                                <li class="card-text">• Mr. Melanio Tomas</li>
+                                <li class="card-text">• Mr. Generoso Tomas</li>
+                                <li class="card-text">• Gerard Ashlee Jose</li>
+                            </ul>
+                            <ul>
+                                <li class="card-text">• Sebastian Gabriel Co</li>
+                                <li class="card-text">• Kean Henderson Tan</li>
+                                <li class="card-text">• Waylan Kantrel Guiang</li>
+                                <li class="card-text">• Rham Axcel Acoba</li>
+                                <li class="card-text">• Alexander James Acebo</li>
+                                <li class="card-text">• Charl Rotsen Lucero</li>
+                                <li class="card-text">• Jathniel Manangbao</li>
+                                <li class="card-text">• Joshua Nashley Pascua</li>
+                                <li class="card-text">• Mr. Uldarico Rosario Jr.</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="card text-center card-gold">
+                    <img class="card-img-top" src="{{ asset('images/candles.jpg') }}" alt="" loading="lazy">
+                    <div class="card-body">
+                        <h5>18 Candles </h5>
+                        <div class="spacer-32"></div>
+                        <div class="participants">
+                            <ul>
+                                <li class="card-text">• Nina Joycel Rosario</li>
+                                <li class="card-text">• Christine Claire Bringas</li>
+                                <li class="card-text">• CJ Simbol</li>
+                                <li class="card-text">• Deanne Hannah Sahagun</li>
+                                <li class="card-text">• Angelia Christine Realda</li>
+                                <li class="card-text">• Precious Mary Asunto</li>
+                                <li class="card-text">• Janelle Rycha Seguerra</li>
+                                <li class="card-text">• Eizzle Joie Felix</li>
+                                <li class="card-text">• Mrs. Alellin Felix</li>
+                            </ul>
+                            <ul>
+                                <li class="card-text">• Gianna Marithe Maynes</li>
+                                <li class="card-text">• Jasmine Venus Domingo</li>
+                                <li class="card-text">• Ms. Preciousa Ramos</li>
+                                <li class="card-text">• Ayessha Mae Daoang</li>
+                                <li class="card-text">• Elijah Joeleeh Felipe</li>
+                                <li class="card-text">• Jinjer Asis</li>
+                                <li class="card-text">• PAndrew Kazantzis</li>
+                                <li class="card-text">• Chelize Abasta</li>
+                                <li class="card-text">• Lovella Rosario</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="card text-center card-maroon">
+                    <img class="card-img-top" src="{{ asset('images/bill.webp') }}" alt="" loading="lazy">
+                    <div class="card-body">
+                        <h5>18 Treasures </h5>
+                        <div class="spacer-32"></div>
+                        <div class="participants">
+                            <ul>
+                                <li class="card-text">• Gianne Francheska Ranche</li>
+                                <li class="card-text">• Gianne Iris Corpuz</li>
+                                <li class="card-text">• Lizza Mae Dela Cruz</li>
+                                <li class="card-text">• Guillana Tayson</li>
+                                <li class="card-text">• Ashby James Arcangel</li>
+                                <li class="card-text">• Melissa Denisse Doropan</li>
+                                <li class="card-text">• Dolly Shanice Manding</li>
+                                <li class="card-text">• Louraine Ifurung</li>
+                                <li class="card-text">• Ranielle Jethroe Alipio</li>
+                            </ul>
+                            <ul>
+                                <li class="card-text">• John Edward Pascual</li>
+                                <li class="card-text">• Charlotte Mae Tomas</li>
+                                <li class="card-text">• Xandara Gacho</li>
+                                <li class="card-text">• Neva Manding</li>
+                                <li class="card-text">• Benjamin Hanson Juan</li>
+                                <li class="card-text">• Clyde Sinang</li>
+                                <li class="card-text">• Marianne Bartolome</li>
+                                <li class="card-text">• Charmel Balicoco</li>
+                                <li class="card-text">• Angel Louella Avila</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="card text-center  card-gold">
+                    <img class="card-img-top" src="{{ asset('images/bill.webp') }}" alt="" loading="lazy">
+                    <div class="card-body">
+                        <h5>18 Paper Bills </h5>
+                        <div class="spacer-32"></div>
+                        <div class="participants">
+                            <ul>
+                                <li class="card-text">• Nina Joycel Rosario & Engr. Aldrin Dela Cruz</li>
+                                <li class="card-text">• Lovella Rosario & Engr. Daniel Despuig</li>
+                                <li class="card-text">• Dr. Gloria Tomas & Engr. Manuel Ferrer</li>
+                                <li class="card-text">• Mr. & Mrs. Rogelio P. Tomas</li>
+                                <li class="card-text">• Mr. & Mrs. Melanio P. Tomas</li>
+                                <li class="card-text">• Mr. & Mrs. Alexander P. Tomas</li>
+                                <li class="card-text">• Dr. Mark Anthony Tomas & Ms. Christel Joy Mariano</li>
+                                <li class="card-text">• Dr. Alelyn D. Tomas</li>
+                                <li class="card-text">• Archt. Jefferson D. Tomas</li>
+                            </ul>
+                            <ul>
+                                <li class="card-text">• Mr. & Mrs. Arcadio Gregorio</li>
+                                <li class="card-text">• Mr. & Mrs. Santos G. Pascual</li>
+                                <li class="card-text">• Kagawad and Mrs. Jessie Jacinto</li>
+                                <li class="card-text">• Ms. Desiree Estavillo</li>
+                                <li class="card-text">• Ms. Michelle Benito</li>
+                                <li class="card-text">• Ms. Grace Pascual</li>
+                                <li class="card-text">• Mrs. Norma Pascual</li>
+                                <li class="card-text">• Mr. Uldarico Rosario Jr.</li>
+                                <li class="card-text">• Mrs. Charito Rosario</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="card text-center card-maroon">
+                    <img class="card-img-top" src="{{ asset('images/wine.jpg') }}" alt="" loading="lazy">
+                    <div class="card-body">
+                        <h5>18 shots</h5>
+                        <div class="spacer-32"></div>
+                        <div class="participants">
+                            <ul>
+                                <li class="card-text">• Mr. Sebastian Gabriel Co</li>
+                                <li class="card-text">• Mr. Kean Henderson Tan</li>
+                                <li class="card-text">• Mr. Waylan Kantrel Guiang</li>
+                                <li class="card-text">• Mr. Rham Axcel Acoba</li>
+                                <li class="card-text">• Ms. Angelia Christine Realda</li>
+                                <li class="card-text">• Ms. Kim Clouie Juan</li>
+                                <li class="card-text">• Ms. Athea Xanthe Alyn Pacada</li>
+                                <li class="card-text">• Ms. Angela Morales</li>
+                                <li class="card-text">• Mr. Aubrey James Juan</li>
+                            </ul>
+                            <ul>
+                                <li class="card-text">• Mr. Gardner Jeff Domingo</li>
+                                <li class="card-text">• Mr. Karl Nino Ulnagan</li>
+                                <li class="card-text">• Ms. Guillana Tayson</li>
+                                <li class="card-text">• Ms. Aira Vanessa Angcao</li>
+                                <li class="card-text">• Ms. Raechelle Paranada</li>
+                                <li class="card-text">• Ms. Crystal Faith Domingo</li>
+                                <li class="card-text">• Ms. Christine Claire Bringas</li>
+                                <li class="card-text">• Ms. Ralph Laurence Ranjo</li>
+                                <li class="card-text">• Mr. Gio Ballesteros</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
-      </div>
     </div>
-    <div>
-      <div class="card text-center card-gold">
-      <img  class="card-img-top" src="{{ asset('images/candles.jpg') }}" alt="" loading="lazy">
-
-        <div class="card-body">
-          <h5>18 Candles </span>
-          <div class="spacer-32"></div>
-          </h5>
-
-          <div class="participants">
-            <ul>
-                <li class="card-text">Nina Joycel Rosario</li>
-                <li class="card-text">Christine Claire Bringas</li>
-                <li class="card-text">CJ Simbol</li>
-                <li class="card-text">Deanne Hannah Sahagun</li>
-                <li class="card-text">Angelia Christine Realda</li>
-                <li class="card-text">Precious Mary Asunto</li>
-                <li class="card-text">Janelle Rycha Seguerra</li>
-                <li class="card-text">Eizzle Joie Felix</li>
-                <li class="card-text">Mrs. Alellin Felix</li>
-            </ul>
-
-            <ul>
-                <li class="card-text">Gianna Marithe Maynes</li>
-                <li class="card-text">Jasmine Venus Domingo</li>
-                <li class="card-text">Ms. Preciousa Ramos</li>
-                <li class="card-text">Ayessha Mae Daoang</li>
-                <li class="card-text">Elijah Joeleeh Felipe</li>
-                <li class="card-text">Jinjer Asis</li>
-                <li class="card-text">PAndrew Kazantzis</li>
-                <li class="card-text">Chelize Abasta</li>
-                <li class="card-text">Lovella Rosario</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div>
-      <div class="card text-center card-maroon">
-      <img  class="card-img-top" src="{{ asset('images/bill.webp') }}" alt="" loading="lazy">
-
-        <div class="card-body">
-          <h5>18 Treasures </span>
-          <div class="spacer-32"></div>
-          </h5>
-
-          <div class="participants">
-            <ul>
-                <li class="card-text">Gianne Francheska Ranche</li>
-                <li class="card-text">Gianne Iris Corpuz</li>
-                <li class="card-text">Lizza Mae Dela Cruz</li>
-                <li class="card-text">Guillana Tayson</li>
-                <li class="card-text">Ashby James Arcangel</li>
-                <li class="card-text">Melissa Denisse Doropan</li>
-                <li class="card-text">Dolly Shanice Manding</li>
-                <li class="card-text">Louraine Ifurung</li>
-                <li class="card-text">Ranielle Jethroe Alipio</li>
-            </ul>
-
-            <ul>
-                <li class="card-text">John Edward Pascual</li>
-                <li class="card-text">Charlotte Mae Tomas</li>
-                <li class="card-text">Xandara Gacho</li>
-                <li class="card-text">Neva Manding</li>
-                <li class="card-text">Benjamin Hanson Juan</li>
-                <li class="card-text">Clyde Sinang</li>
-                <li class="card-text">Marianne Bartolome</li>
-                <li class="card-text">Charmel Balicoco</li>
-                <li class="card-text">Angel Louella Avila</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div>
-      <div class="card text-center  card-gold">
-      <img  class="card-img-top" src="{{ asset('images/bill.webp') }}" alt="" loading="lazy">
-
-        <div class="card-body">
-          <h5>18 Paper Bills </span>
-          <div class="spacer-32"></div>
-          </h5>
-
-          <div class="participants">
-            <ul>
-                <li class="card-text">Nina Joycel Rosario & Engr. Aldrin Dela Cruz</li>
-                <li class="card-text">Lovella Rosario & Engr. Daniel Despuig</li>
-                <li class="card-text">Dr. Gloria Tomas & Engr. Manuel Ferrer</li>
-                <li class="card-text">Mr. & Mrs. Rogelio P. Tomas</li>
-                <li class="card-text">Mr. & Mrs. Melanio P. Tomas</li>
-                <li class="card-text">Mr. & Mrs. Alexander P. Tomas</li>
-                <li class="card-text">Dr. Mark Anthony Tomas & Ms. Christel Joy Mariano</li>
-                <li class="card-text">Dr. Alelyn D. Tomas</li>
-                <li class="card-text">Archt. Jefferson D. Tomas</li>
-            </ul>
-
-            <ul>
-                <li class="card-text">Mr. & Mrs. Arcadio Gregorio</li>
-                <li class="card-text">Mr. & Mrs. Santos G. Pascual</li>
-                <li class="card-text">Kagawad and Mrs. Jessie Jacinto</li>
-                <li class="card-text">Ms. Desiree Estavillo</li>
-                <li class="card-text">Ms. Michelle Benito</li>
-                <li class="card-text">Ms. Grace Pascual</li>
-                <li class="card-text">Mrs. Norma Pascual</li>
-                <li class="card-text">Mr. Uldarico Rosario Jr.</li>
-                <li class="card-text">Mrs. Charito Rosario</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div>
-      <div class="card text-center card-maroon">
-      <img  class="card-img-top" src="{{ asset('images/wine.jpg') }}" alt="" loading="lazy">
-
-        <div class="card-body">
-          <h5>18 shots</h5>
-          <div class="spacer-32"></div>
-
-          <div class="participants">
-            <ul>
-                <li class="card-text">Mr. Sebastian Gabriel Co</li>
-                <li class="card-text">Mr. Kean Henderson Tan</li>
-                <li class="card-text">Mr. Waylan Kantrel Guiang</li>
-                <li class="card-text">Mr. Rham Axcel Acoba</li>
-                <li class="card-text">Ms. Angelia Christine Realda</li>
-                <li class="card-text">Ms. Kim Clouie Juan</li>
-                <li class="card-text">Ms. Athea Xanthe Alyn Pacada</li>
-                <li class="card-text">Ms. Angela Morales</li>
-                <li class="card-text">Mr. Aubrey James Juan</li>
-            </ul>
-
-            <ul>
-                <li class="card-text">Mr. Gardner Jeff Domingo</li>
-                <li class="card-text">Mr. Karl Nino Ulnagan</li>
-                <li class="card-text">Ms. Guillana Tayson</li>
-                <li class="card-text">Ms. Aira Vanessa Angcao</li>
-                <li class="card-text">Ms. Raechelle Paranada</li>
-                <li class="card-text">Ms. Crystal Faith Domingo</li>
-                <li class="card-text">Ms. Christine Claire Bringas</li>
-                <li class="card-text">Ms. Ralph Laurence Ranjo</li>
-                <li class="card-text">Mr. Gio Ballesteros</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-
-  </div>
-</div>
     <div class="spacer-40"></div>
     <div class="spacer-40"></div>
 </div>
-
-<style>
-    .participants ul {
-    list-style-type: disc; /* Use disc bullets */
-}
-</style>
